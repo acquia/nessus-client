813e0e38cd683c9ffac68b9c9d79a621fdddd349
peter.wolanin <peter.wolanin@acquia.com>
2016-04-19 13:58:17 -0400

    re-ord code and add policy

--- lib/nessus-client/cli/policy.rb
+++ lib/nessus-client/cli/policy.rb
@@ -1,5 +1,23 @@
-class NessusCLI
-
-  class Policy < NessusCLIBase
+module NessusCLI
+  module Commands
+    class Policy < NessusCLI::Base
+  
+      desc "policy list", "List scans since a given date"
+      method_option :columns, :aliases => '-c', :type => :array, :default => %w(id name owner visibility), :desc => 'List of columns to display in a table'
+      self.common_options
+      def list
+        client = self.class.client(options[:home])
+        result = client.get('/policies')
+        # Protect against empty results.
+        result['policies'] ||= []
+        self.class.table_for(result['policies'], options['columns'], "Policies") do |scan|
+          options['columns'].map { |column| (column.match('date') && scan[column].is_a?(Integer)) ? Time.at(scan[column]).to_s : scan[column] }
+        end
+        if result['policies'].length > 0
+          columns = result['policies'].first.keys
+          say("Available columns:\n" + columns.join(', '))
+        end
+      end
+    end
   end
 end
